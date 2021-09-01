dae4a7dda5600c5f9f0103cfb13381031f0e145b
Borislav G. Popov <65204241+brsvppv@users.noreply.github.com>
2021-09-01 17:22:17 +0300

    Changing Comments

--- CreateBulkNavUsersForm.ps1
+++ CreateBulkNavUsersForm.ps1
@@ -1,38 +1,4 @@
-﻿##[Ps1 To Exe]
-##
-##Kd3HDZOFADWE8uO1
-##Nc3NCtDXTlCDjvPxzAtZyn/Rb0cdS/myurmp173trLq46nKAGcxEdl9nmSL5FgW0Wv1y
-##Kd3HFJGZHWLWoLaVvnQnhQ==
-##LM/RF4eFHHGZ7/K1
-##K8rLFtDXTiW5
-##OsHQCZGeTiiZ4dI=
-##OcrLFtDXTiW5
-##LM/BD5WYTiiZ4tI=
-##McvWDJ+OTiiZ4tI=
-##OMvOC56PFnzN8u+Vs1Q=
-##M9jHFoeYB2Hc8u+Vs1Q=
-##PdrWFpmIG2HcofKIo2QX
-##OMfRFJyLFzWE8uK1
-##KsfMAp/KUzWJ0g==
-##OsfOAYaPHGbQvbyVvnQX
-##LNzNAIWJGmPcoKHc7Do3uAuO
-##LNzNAIWJGnvYv7eVvnQX
-##M9zLA5mED3nfu77Q7TV64AuzAgg=
-##NcDWAYKED3nfu77Q7TV64AuzAgg=
-##OMvRB4KDHmHQvbyVvnQX
-##P8HPFJGEFzWE8tI=
-##KNzDAJWHD2fS8u+Vgw==
-##P8HSHYKDCX3N8u+Vgw==
-##LNzLEpGeC3fMu77Ro2k3hQ==
-##L97HB5mLAnfMu77Ro2k3hQ==
-##P8HPCZWEGmaZ7/K1
-##L8/UAdDXTlCDjpH5zAFT2X/rQ2VrWOyokJmJhMz83d/AnATrYLtUZUBzqhrdSWa8V/MVUPgQusVRGF0ZLOAC8qbDLOi7SaY209F6auGXlbE7HErM8K+1/Tik877SGw12U37qYKlxFD3u12TPWFG5m4FokWabU9jUuJt0pn2K2HEG30dFUK+Ek1k=
-##Kc/BRM3KXxU=
-##
-##
-##fd6a9f26a06ea3bc99616d4851b372ba
-
-Add-Type -AssemblyName System.Windows.Forms
+﻿Add-Type -AssemblyName System.Windows.Forms
 [void][System.Reflection.Assembly]::LoadWithPartialName('presentationframework')
 [xml]$XAML = @"
 <Window
