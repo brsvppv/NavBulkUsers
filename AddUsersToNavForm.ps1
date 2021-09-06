04c4f24957e2d7bddbc6ecadf5956547bf3efac8
Borislav G. Popov <65204241+brsvppv@users.noreply.github.com>
2021-09-06 04:04:51 +0300

    AddingRemoveButton

--- AddUsersToNavForm.ps1
+++ AddUsersToNavForm.ps1
@@ -44,7 +44,7 @@ xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
         xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
      
         Title="Create NavUsers From File" Height="365" Width="350" ResizeMode="NoResize" WindowStartupLocation="CenterScreen" ShowInTaskbar="True">
-        <Grid Background="{DynamicResource {x:Static SystemColors.WindowBrushKey}}" Height="365" Width="345" VerticalAlignment="Center" HorizontalAlignment="Center">
+        <Grid Background="{DynamicResource {x:Static SystemColors.WindowBrushKey}}" Height="365" VerticalAlignment="Center" HorizontalAlignment="Center" Width="344">
             <TextBox Name="txtUserFile" HorizontalAlignment="Left" Height="20" Width="311" Margin="5,31,0,0" Text="Select Username File List" VerticalAlignment="Top"  FontSize="11" IsReadOnly="true"/>
             <Label Name="lblsrvc" Content="Service Instance" HorizontalAlignment="Left" Margin="3,105,0,0" VerticalAlignment="Top"/>
             <ComboBox Name="cbxNavInstance" HorizontalAlignment="Left" Margin="128,105,0,0" VerticalAlignment="Top" Width="213" SelectedIndex="0"/>
@@ -64,7 +64,8 @@ xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
             <Label Name="lblAuthType" Content="Authentication " HorizontalAlignment="Left" Margin="3,80,0,0" VerticalAlignment="Top"/>
             <ListBox Name="listBoxPermission" Width="160"  Height="150" Margin="5,163,179,42"/>
             <ListBox Name="listBoxPermissionSelected" Width="160"  Height="150" Margin="0,163,5,42" HorizontalAlignment="right"/>
-            <Button Name="btnAddPerm" Content=">" Margin="0,168,0,0" VerticalAlignment="Top" Height="150" HorizontalAlignment="center" Width="16"/>
+            <Button Name="btnAddPerm" Content=">" Margin="0,167,0,0" VerticalAlignment="Top" Height="75" HorizontalAlignment="Center" Width="16"/>
+            <Button Name="btnRemoveSelcted" Content="&gt;" Margin="0,242,0,0" VerticalAlignment="Top" Height="75" HorizontalAlignment="Center" Width="16"/>      
         </Grid>
 </Window>
 "@
@@ -143,7 +144,6 @@ else {
         Import-Module "$globaldir\NavAdminTool.ps1" -ErrorAction Stop
         [System.Windows.MessageBox]::Show("No Server Installation:  $_", 'Error No Install', 'OK', 'Error')
     }
-    $intVersion = [int]::Parse($vdir)
 }
 $Form.Add_Loaded( {       
         
@@ -272,12 +272,16 @@ $btnSavePswdFile.Add_click( {
         SelectDirectory
         #$filepath = $txtPswdFile.Text
     })
-$btnAddPerm.Add_Click( {
+$btnAddPerm.Add_Click({
         $PermSelected = $listBoxPermission.SelectedItem
         $listBoxPermission.Items.Remove($PermSelected)
         $listBoxPermissionSelected.Items.Add($PermSelected)
     
     })
+$btnRemoveSelcted.Add_Click({
+    $listBoxPermission.Items.Add($listBoxPermissionSelected.Selecteditem)
+    $listBoxPermissionSelected.Items.Remove($listBoxPermissionSelected.Selecteditem)
+})
 $btnPerformAction.Add_click( {
 
         $navUserNames = (Get-Content -Path $txtUserFile.Text)       
